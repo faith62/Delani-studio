0d0f208ab73c15318083e9f5e541b1675c116747
faith62 <phifeichemu@gmail.com>
2022-03-13 22:33:28 +0300

    upgrade jquery, javascript link

--- js/script.js
+++ js/script.js
@@ -1,13 +1,37 @@
 $(document).ready(function(){
-    $("#designIcon").click(function(){
+    $("#design").click(function(){
         $("#design-content").show(function(){
-          $("#designIcon").hide('fast');
+          $("#design").hide('fast');
         
         });
     });
     $("#design-content").click(function(){
-        $("#designIcon").show(function(){
+        $("#design").show(function(){
           $("#design-content").hide('fast');
+        
+        });
+    });
+    $("#dev").click(function(){
+        $("#dev-content").show(function(){
+          $("#dev").hide('fast');
+        
+        });
+    });
+    $("#dev-content").click(function(){
+        $("#dev").show(function(){
+          $("#dev-content").hide('fast');
+        
+        });
+    });
+    $("#product").click(function(){
+        $("#product-content").show(function(){
+          $("#product").hide('fast');
+        
+        });
+    });
+    $("#product-content").click(function(){
+        $("#product").show(function(){
+          $("#product-content").hide('fast');
         
         });
     });
